e3d8b21f57d3a1f7fd9135aa28f3c78553523371
Johndhee Ballesteros <Johndheeballesteros13@gmail.com>
2022-05-22 06:30:52 -0700

    made  card link to product.id

--- src/Components/Products/ProductCard.js
+++ src/Components/Products/ProductCard.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
+
 function ProductCard({product}) {
+
+
+
   return (
-   <Card style={{ width: '18rem' }}>
+   <Card as={Link} to={`/products/${product.id}`} style={{ width: '18rem' }}>
     <Card.Img variant="top" src={product.image} />
     <Card.Body>
       <Card.Title>{product.name}</Card.Title>
       <Card.Text>{product.price}</Card.Text>
-      <Button variant="primary">Details</Button>
+      {/* <Button variant="primary">Details</Button> */}
     </Card.Body>
    </Card>
   )
